ee68edd47e7d2352d23daa9b874e545d4cc4a11c
Juan Espin <juan_espin1995@hotmail.es>
2025-08-20 18:46:27 -0500

    feat(socials): Create socials component

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+"use client";
 
-const Socials = () => {
+import {
+  RiLinkedinFill,
+  RiBehanceFill,
+  RiFacebookFill,
+  RiInstagramFill,
+} from "react-icons/ri";
+
+import { FaDailymotion } from "react-icons/fa";
+
+import Link from "next/link";
+
+//
+const icons = [
+  {
+    path: "/",
+    name: <RiLinkedinFill />,
+  },
+  {
+    path: "/",
+    name: <RiBehanceFill />,
+  },
+  {
+    path: "/",
+    name: <RiFacebookFill />,
+  },
+  {
+    path: "/",
+    name: <RiInstagramFill />,
+  },
+  {
+    path: "/",
+    name: <FaDailymotion />,
+  },
+];
+
+const Socials = ({ containerStyles, iconStyles }) => {
   return (
-    <div>Socials</div>
-  )
-}
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => (
+        <Link href={icon.path} key={index}>
+          <div className={`${iconStyles}`}>{icon.name}</div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
-export default Socials
+export default Socials;
