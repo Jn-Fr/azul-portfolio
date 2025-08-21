e200a6f2549f921be76a4e61191de84bf86e7d78
Juan Espin <juan_espin1995@hotmail.es>
2025-08-20 19:33:40 -0500

    feat(badge): Create badge components in hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -49,7 +49,37 @@ const Hero = () => {
             />
           </div>
           {/* image */}
-          <div className="hidden xl:flex relative">image</div>
+          <div className="hidden xl:flex relative">
+            {/* badge 1 */}
+            <Badge
+              containerStyles="absolute top-[24%] -left-[5rem]"
+              icon={<RiBriefcase4Fill />}
+              endCountNum={7}
+              badgeText="Years of Experience"
+            />
+            {/* badge 2 */}
+            <Badge
+              containerStyles="absolute top-[80%] -left-[1rem]"
+              icon={<RiTodoFill />}
+              endCountNum={1}
+              endCountText="k"
+              badgeText="Finished projects"
+            />
+            {/* badge 3 */}
+            <Badge
+              containerStyles="absolute top-[55%] -right-8"
+              icon={<RiTeamFill />}
+              endCountNum={9}
+              endCountText="k"
+              badgeText="Happy Clients"
+            />
+
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc="/hero/developer.png"
+            />
+          </div>
         </div>
         {/* icon */}
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
