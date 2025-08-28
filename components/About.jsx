efb4a6e95e371d071675f3422bf4a06d50d47bb5
Juan Espin <juan_espin1995@hotmail.es>
2025-08-28 16:49:22 -0500

    feat(project card section): Finish Work section with Project Card component

--- components/About.jsx
+++ components/About.jsx
@@ -186,7 +186,7 @@ const About = () => {
                     <div className="flex flex-col gap-y-2">
                       <div className="text-primary ">Language Skill</div>
                       <div className="border-b border-border"></div>
-                      <div>English, Spanish</div>
+                      <div>Spanish, English</div>
                     </div>
                   </div>
                 </TabsContent>
@@ -308,7 +308,7 @@ const About = () => {
                           const { imgPath } = item;
 
                           return (
-                            <div>
+                            <div key={index}>
                               <Image
                                 src={imgPath}
                                 width={48}
